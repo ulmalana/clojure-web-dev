e6680c8b133868816304c64d577ee594b862bfa2
rz <muhamaul@gmail.com>
2022-10-05 15:58:29 +0700

    [webdev] finished chapter 1

--- ch01-get-started/guestbook/src/clj/guestbook/routes/home.clj
+++ ch01-get-started/guestbook/src/clj/guestbook/routes/home.clj
@@ -5,18 +5,47 @@
    [clojure.java.io :as io]
    [guestbook.middleware :as middleware]
    [ring.util.response]
-   [ring.util.http-response :as response]))
+   [ring.util.http-response :as response]
+   [struct.core :as st]))
 
-(defn home-page [request]
-  (layout/render request "home.html" {:docs (-> "docs/docs.md" io/resource slurp)}))
+(def message-schema
+  [[:name
+    st/required
+    st/string]
+   [:message
+    st/required
+    st/string
+    {:message "message must contain at least 10 characters"
+     :validate (fn [msg] (>= (count msg) 10))}]])
+
+(defn home-page [{:keys [flash] :as request}]
+  (layout/render
+   request
+   "home.html"
+   (merge {:messages (db/get-messages)}
+          (select-keys flash [:name :message :errors]))))
 
 (defn about-page [request]
   (layout/render request "about.html"))
 
+(defn validate-message [params]
+  (first (st/validate params message-schema)))
+
+(defn save-message! [{:keys [params]}]
+  (if-let [errors (validate-message params)]
+    (-> (response/found "/")
+        (assoc :flash (assoc params :errors errors)))
+
+    (do
+      (db/save-message! params)
+      (response/found "/"))))
+
+
 (defn home-routes []
-  [ "" 
+  [ ""
    {:middleware [middleware/wrap-csrf
                  middleware/wrap-formats]}
    ["/" {:get home-page}]
-   ["/about" {:get about-page}]])
+   ["/about" {:get about-page}]
+   ["/message" {:post save-message!}]])
 
