39b0c39410574ecd21e6e3dff4b23bd41649952d
rz <muhamaul@gmail.com>
2022-12-10 17:09:57 +0700

    [webdev] started chapter 9

--- ch09-deployment/guestbook/test/clj/guestbook/db/core_test.clj
+++ ch09-deployment/guestbook/test/clj/guestbook/db/core_test.clj
@@ -29,3 +29,13 @@
            (-> (db/get-messages t-conn {})
                (first)
                (select-keys [:name :message]))))))
+
+(deftest test-users
+  (jdbc/with-transaction [t-conn *db* {:rollback-only true}]
+    (is (= 1 (db/create-user!* t-conn
+                               {:login "foo"
+                                :password "password"})))
+    (is (= {:login "foo"
+            :profile {}}
+           (dissoc (db/get-user* t-conn
+                                 {:login "foo"}) :created_at)))))
--- ch09-deployment/guestbook/test/clj/guestbook/handler_test.clj
+++ ch09-deployment/guestbook/test/clj/guestbook/handler_test.clj
@@ -5,7 +5,9 @@
     [guestbook.handler :refer :all]
     [guestbook.middleware.formats :as formats]
     [muuntaja.core :as m]
-    [mount.core :as mount]))
+    [mount.core :as mount]
+    [buddy.hashers :as hashers]
+    [clojure.instant :refer [read-instant-date]]))
 
 (defn parse-json [body]
   (m/decode formats/instance "application/json" body))
@@ -14,7 +16,8 @@
   :once
   (fn [f]
     (mount/start #'guestbook.config/env
-                 #'guestbook.handler/app-routes)
+                 #'guestbook.routes.websockets/socket
+                 #'guestbook.handler/routes)
     (f)))
 
 (deftest test-app
@@ -25,3 +28,35 @@
   (testing "not-found route"
     (let [response ((app) (request :get "/invalid"))]
       (is (= 404 (:status response))))))
+
+(defn login-request [login pass]
+  (-> (request :post "/api/login")
+      (json-body {:login login
+                  :password pass})))
+
+(def now (java.util.Date. 0))
+
+(defn mock-user [{:keys [login]}]
+  (when (= login "foo")
+    {:login "foo"
+     :password (hashers/encrypt "password")
+     :created_at now
+     :profile {}}))
+
+(deftest test-login
+  (with-redefs [guestbook.db.core/get-user-for-auth* mock-user]
+    (let [handler (app)]
+      (testing "login success"
+        (let [{:keys [body status] :as r} (handler
+                                           (login-request "foo" "password"))
+              json (parse-json body)]
+          (println (assoc r :body json))
+          (is (= 200 status))
+          (is (= (-> (:identity json)
+                     (update :created_at read-instant-date))
+                 (-> {:login "foo"}
+                      mock-user
+                      (dissoc :password))))))
+      (testing "password mismatch"
+        (let [{:keys [status]} (handler (login-request "foo" "hacker"))]
+          (is (= 401 status)))))))
